91ea9f542fd004c07fe73b72ecabece460a5f8e8
anyawebb <57605199+anyawebb@users.noreply.github.com>
2021-09-09 19:42:24 +1200

    Sticky nav in browser pages
    Added the sticky nav after scroll to the dispatcher navigation

--- PracTui/JS/ui.js
+++ PracTui/JS/ui.js
@@ -19,3 +19,18 @@ document.getElementById("todaysDate").innerHTML = d.toDateString();
 // document.getElementById("clear").addEventListener('click', function(){
 // signaturePad.clear();
 // })
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 370) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
